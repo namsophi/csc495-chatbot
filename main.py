9262d1f4d1d13345aaa25e6cea5efb3a69fa52b3
snam <s.nam@mail.utoronto.ca>
2022-06-02 23:51:35 -0400

    Add nltk to classify statements

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from gtts import gTTS
 import speech_recognition as sr
-import numpy as np
 import os
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('popular', quiet=True)
+nltk.download('nps_chat', quiet=True)
+nltk.download('punkt')
+nltk.download('wordnet')
 
 
-class ChatBot():
+class ChatBot:
     def __init__(self, name):
         print("--- starting up", name, "---")
         self.name = name
+        self.posts = nltk.corpus.nps_chat.xml_posts()[:10000]
+        self.feature_sets = [(self.dialogue_act_features(post.text),
+                              post.get('class')) for post in self.posts]
+        self.size = int(len(self.feature_sets) * 0.1)
+        self.train_set, self.test_set = self.feature_sets[self.size:], \
+            self.feature_sets[:self.size]
+        self.classifier = nltk.NaiveBayesClassifier.train(self.train_set)
+
+    # To Recognise input type as QUES.
 
     def speech_to_text(self):
         recognizer = sr.Recognizer()
@@ -15,33 +30,39 @@ class ChatBot():
             print("listening...")
             audio = recognizer.listen(mic)
         try:
-            self.text = recognizer.recognize_google(audio)
-            print("me --> ", self.text)
-        except:
-            print("me -->  ERROR")
-
-    def wake_up(self, text):
-        return True if self.name in text.lower() else False
+            user_response = format(recognizer.recognize_google(audio))
+            print("me --> ", user_response)
+        except sr.UnknownValueError:
+            ai.text_to_speech("Oops! Didn't catch that")
+            pass
+        clas = self.classifier.classify(
+            self.dialogue_act_features(user_response))
+        if clas != 'Bye':
+            if clas == 'Emotion':
+                ai.text_to_speech("You're welcome!")
+                return False
+        else:
+            ai.text_to_speech("Bye! take care.")
+            return False
 
     @staticmethod
     def text_to_speech(text):
         print("ai --> ", text)
         speaker = gTTS(text=text, lang="en", slow=False)
         speaker.save("res.mp3")
-        os.system("afplay res.mp3")  # macbook->afplay | windows->start
+        os.system("afplay res.mp3")
         os.remove("res.mp3")
+
+    @staticmethod
+    def dialogue_act_features(post):
+        features = {}
+        for word in nltk.word_tokenize(post):
+            features['contains({})'.format(word.lower())] = True
+        return features
 
 
 if __name__ == "__main__":
     ai = ChatBot(name="maya")
-
-    while True:
-        ai.speech_to_text()
-
-        if ai.wake_up(ai.text) is True:
-            res = "Hello I am Maya the AI, what can I do for you?"
-
-        elif any(i in ai.text for i in ["thank","thanks"]):
-            res = np.random.choice(["you're welcome!","anytime!","no problem!","cool!","I'm here if you need me!","peace out!"])
-
-        ai.text_to_speech(res)
+    flag = True
+    while flag:
+        flag = ai.speech_to_text()
